1e66d70f5a17172a0b4fad49565b3ecb8c523e24
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-10-27 12:02:00 +0200

    ok

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AppCreateKittenComponent } from './app-create-kitten/app-create-kitten.component';
+import { AppListKittenComponent } from './app-list-kitten/app-list-kitten.component';
+import { AppUserKittenComponent } from './app-user-kitten/app-user-kitten.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppCreateKittenComponent,
+    AppListKittenComponent,
+    AppUserKittenComponent,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
